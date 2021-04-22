684d97bf0d25e96a6562f03e1c05086ba61c5ffb
maker2413 <maker2413@gmail.com>
2021-04-22 12:21:00 -0700

    created simple flask app to demo docker

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "The app works! Please git the '<ip>/hello/<your name>' endpoint to see it in action!\n"
+
+@app.route('/hello/<user>')
+def hello(user):
+    return jsonify(response= "Hello there, %s" user)
