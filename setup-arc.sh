d731ab9303ce66d0fbbe7df6e20d09994f30fa7a
maker2413 <maker2413@gmail.com>
2026-03-13 22:56:47 -0500

    fixed setup script

--- setup-arc.sh
+++ setup-arc.sh
@@ -2,6 +2,11 @@
 
 read -p "What is your GitHub PAT: " pat;
 
+echo "apiVersion: v1
+kind: Namespace
+metadata:
+  name: arc-runners" | kubectl apply -f -
+
 kubectl create secret generic github-pat \
    --namespace=arc-runners \
    --from-literal=github_token=$pat;
